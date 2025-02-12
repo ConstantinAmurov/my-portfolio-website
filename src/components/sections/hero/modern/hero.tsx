7b09e2318c3ccfc54b1cc8626328ef1a5a16b070
Constantin Amurov <constantin.amurov@assist.ro>
2025-02-12 11:14:21 +0200

    fix: improve hero banner styling on large screens

--- src/components/sections/hero/modern/hero.tsx
+++ src/components/sections/hero/modern/hero.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import React, { useRef } from 'react';
-import TextReveal from '@/components/motion/text-reveal';
 import ParallaxImage from '@/components/motion/parallax-image';
 import { TextLoop } from '@/components/motion/text-loop';
-
+import styles from "./style.module.scss"
+import { styleText } from 'util';
 const heroContent = [
   { role: 'developer', action: 'code', emoji: '💻', bgColor: 'bg-green-100' },
   { role: 'problem solver', action: 'debug', emoji: '🛠️', bgColor: 'bg-gray-100' },
@@ -34,10 +34,11 @@ function Hero() {
       className="relative w-full overflow-hidden bg-background/[0.96]"
       ref={container}
     >
-      <div className="relative z-10 h-[42.5dvh] md:h-[51.2dvh] md:min-h-[50dvh] xl:h-[61.2dvh]">
+      <div className="h-12"></div>
+      <div className="relative z-10 h-[20dvh] sm:h-[30dvh]">
         <div className="relative flex h-full flex-col items-center justify-center">
           <div className="flex w-full items-center justify-center px-4 md:px-6">
-            <h1 className="text-4xl font-light sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+            <h1 className="text-2xl font-light sm:text-5xl lg:text-6xl">
               <span>A </span>
               <TextLoop
                 className="overflow-y-clip"
@@ -49,19 +50,19 @@ function Hero() {
                 ))}
               </TextLoop>
               <br />
-              <span className="flex items-center gap-2 md:gap-4">
+              <span className="flex items-center gap-2">
                 <span>Who</span>
                 <TextLoop
-                  className="my-auto inline-block h-[3.25rem] overflow-hidden overflow-y-clip md:h-[7.8rem]"
+                  className="my-auto inline-block h-[2.25rem] sm:h-[3.25rem] lg:h-[4.25rem] overflow-hidden overflow-y-clip"
                   transition={textLoopTransition}
                   variants={textLoopVariants}
                 >
                   {heroContent.map((content, index) => (
                     <span
                       key={index}
-                      className={`relative mx-2 my-auto inline-block aspect-[1.5/1] h-[3.25rem] overflow-hidden rounded-full md:mx-4 md:h-[7.8rem] ${content.bgColor}`}
+                      className={`relative mx-2 my-auto inline-block aspect-[1.5/1] h-[2.25rem] sm:h-[3.25rem] lg:h-[4.25rem] overflow-hidden rounded-full ${content.bgColor}`}
                     >
-                      <span className="absolute inset-0 flex select-none items-center justify-center text-4xl md:text-7xl">
+                      <span className="absolute inset-0 flex select-none items-center justify-center text-lg sm:text-3xl lg:text-4xl">
                         {content.emoji}
                       </span>
                     </span>
@@ -82,20 +83,13 @@ function Hero() {
           </div>
         </div>
       </div>
-
-      <ParallaxImage
-        src="/images/IMG_0919.JPG"
-        containerRef={container}
-        alt="Hero image"
-        containerClassName="aspect-video w-screen lg:mt-28"
-        priority
-        parallaxOptions={{
-          yStart: '0%',
-          yEnd: '10%',
-          scaleStart: 1,
-          scaleEnd: 1.5
-        }}
-      />
+      <div className="flex justify-center">
+        <img
+          src="/images/IMG_0919.JPG"
+          alt="Hero image"
+          className='aspect-video w-screen xl:w-[60%] object-cover object-center xl:rounded-2xl'
+        />
+      </div>
     </section>
   );
 }
